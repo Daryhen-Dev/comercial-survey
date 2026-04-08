6e141b0873963c31079ff05ec8ceba6f40ffec81
Daryhen-dev <daryhen.work@gmail.com>
2026-04-07 20:28:45 -0500

    fix: move url to prisma.config.ts only, directUrl used manually for migrations

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'prisma/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
   migrations: {
     seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
   },
